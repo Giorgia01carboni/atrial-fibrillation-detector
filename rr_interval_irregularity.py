27fc6fda4660957d606b90ffd13b8dcfe81d049a
giorgia01carboni <giorgia.carboni@gmail.com>
2025-02-13 00:43:44 +0100

    Point 2.2 commit

--- rr_interval_irregularity.py
+++ rr_interval_irregularity.py
@@ -1,16 +1,21 @@
 import numpy as np
 
 def rr_sliding_window(rr_intervals, N, gamma):
+
     '''
-    Make a window sliding on the rr intervals array and look for irregular intervals
+    Creates a window sliding on the rr intervals array and look for irregular intervals
     by checking against a chosen value gamma (in seconds).
-    :param rr_intervals:
-    :param N: 8-beat sliding window length
-    :param gamma: value used to look for irregular intervals (in seconds)
-    :return greater_than_gamma: number of intervals exceeding gamma
+    :param rr_intervals: array of RR-intervals
+    :param N: Sliding window's length (on the paper: 8 beats)
+    :param gamma: Threshold for detecting irregular intervals (in seconds)
+    :return irregular_pairs: number of intervals exceeding gamma (for each window)
     '''
+
+    irregular_pairs = []
+
     for i in range(len(rr_intervals) - N + 1):
         window = rr_intervals[i: i + N]
-        greater_than_gamma = sum(1 for j in range(window) for k in range(j+1, window) if abs[j - k] > gamma)
+        irregular_pair = sum(1 for j in range(len(window)) for k in range(j+1, len(window)) if abs(window[j] - window[k]) > gamma)
+        irregular_pairs.append(irregular_pair)
 
-    return greater_than_gamma
+    return irregular_pairs
